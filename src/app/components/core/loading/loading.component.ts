7e8d58a9366f026af05dcd155cbfd327a22d64ea
aidilrx04 <mrcrayv14@gmail.com>
2025-02-27 03:28:47 +0800

    add label to loading

--- src/app/components/core/loading/loading.component.ts
+++ src/app/components/core/loading/loading.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   imports: [],
   templateUrl: './loading.component.html',
-  styleUrl: './loading.component.css'
+  styleUrl: './loading.component.css',
 })
 export class LoadingComponent {
-
+  label = input('');
 }
