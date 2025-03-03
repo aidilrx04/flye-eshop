7e5082ea875cf5db21f72e9f384d20c29a4ae43d
aidilrx04 <mrcrayv14@gmail.com>
2025-03-03 14:11:18 +0800

    bob

--- src/app/components/core/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/components/core/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -2,19 +2,36 @@ import { Component, input, signal, viewChildren } from '@angular/core';
 import { NavItemModel } from '../../nav-item/nav-item.model';
 import { NavItemComponent } from '../../nav-item/nav-item.component';
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { NavItemType } from '../../nav-item/nav-item-type';
+import { filter, Observable } from 'rxjs';
+import { UserModel } from '../../../../models/user.model';
+import { AuthService } from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [NavItemComponent, RouterLink, CommonModule],
+  imports: [NavItemComponent, RouterLink, CommonModule, AsyncPipe],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css',
 })
 export class DashboardLayoutComponent {
+  constructor(private authService: AuthService) {}
+
   navItems = input<NavItemModel[]>([]);
+  signOutNavItem: NavItemModel = {
+    type: NavItemType.LINK,
+    label: 'Sign Out',
+    icon: 'ph ph-sign-out',
+    href: '/signout',
+  };
 
   isNavOpen = signal(false);
   navItemRefs = viewChildren(NavItemComponent);
+  user$!: Observable<UserModel>;
+
+  ngOnInit() {
+    this.user$ = this.authService.user$.pipe(filter((v) => v !== null));
+  }
 
   ngAfterViewInit() {
     for (const navItem of this.navItemRefs()) {
