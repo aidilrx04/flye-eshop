4d40a6ce200e5edc04fd989e7f8210814f5c2a4f
aidilrx04 <mrcrayv14@gmail.com>
2025-03-08 10:33:26 +0800

    modal service test

--- src/app/services/modal.service.spec.ts
+++ src/app/services/modal.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ModalService } from './modal.service';
+import { first, firstValueFrom } from 'rxjs';
+import { TemplateRef } from '@angular/core';
 
 describe('ModalService', () => {
   let service: ModalService;
@@ -12,5 +14,30 @@ describe('ModalService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should open a modal', async () => {
+    expect(await firstValueFrom(service.modal$)).toBeNull();
+
+    const template = {} as any as TemplateRef<any>;
+
+    service.openModal(template);
+
+    expect(await firstValueFrom(service.modal$)).toEqual(
+      {} as any as TemplateRef<any>,
+    );
+  });
+  it('should close a modal', async () => {
+    const template = {} as any as TemplateRef<any>;
+
+    service.openModal(template);
+
+    expect(await firstValueFrom(service.modal$)).toEqual(
+      {} as any as TemplateRef<any>,
+    );
+
+    service.closeCurrentModal();
+
+    expect(await firstValueFrom(service.modal$)).toBeNull();
   });
 });
