ac174fd46aa90f4af6f00c5b9b838e803fdc1701
aidilrx04 <mrcrayv14@gmail.com>
2025-03-08 11:06:27 +0800

    user service test

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { environment } from '../../environments/environment';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch a list of users', () => {
+    service.getUsers().subscribe((users) => {
+      expect(users.data).toBeInstanceOf(Array);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/users?`);
+    expect(req.request.method).toEqual('GET');
+    req.flush({ data: [] });
+  });
+
+  it('should delete a user', () => {
+    service.deleteUser(1).subscribe();
+    const req = httpMock.expectOne(`${environment.apiUrl}/users/1`);
+    expect(req.request.method).toEqual('DELETE');
   });
 });
