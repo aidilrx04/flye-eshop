4e56c266ff5790986c0ade5b6c5111a3b94ec773
aidilrx04 <mrcrayv14@gmail.com>
2025-03-08 12:42:57 +0800

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,89 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { ScrollService } from './services/scroll.service';
+import { BehaviorSubject, of } from 'rxjs';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let authServiceMock: jasmine.SpyObj<AuthService>;
+  let routerMock: jasmine.SpyObj<Router>;
+  let scrollServiceMock: jasmine.SpyObj<ScrollService>;
+  let initCompletedSubject: BehaviorSubject<boolean>;
+
   beforeEach(async () => {
+    initCompletedSubject = new BehaviorSubject(false);
+
+    authServiceMock = jasmine.createSpyObj<AuthService>('AuthService', [], {
+      initCompletedSubject,
+    });
+
+    routerMock = jasmine.createSpyObj<Router>('Router', ['events'], {
+      events: of(new NavigationEnd(1, '/', '/')),
+    });
+
+    scrollServiceMock = jasmine.createSpyObj<ScrollService>('ScrollService', [
+      'scrollToTop',
+    ]);
+
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        },
+        {
+          provide: Router,
+          useValue: routerMock,
+        },
+        {
+          provide: ScrollService,
+          useValue: scrollServiceMock,
+        },
+      ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'flye' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    // expect(app.title).toEqual('flye');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should scroll to top when navigation end', () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, flye');
+    expect(scrollServiceMock.scrollToTop).toHaveBeenCalled();
   });
+  it('should wait for init to complete', (done) => {
+    fixture.detectChanges();
+    expect(component.hasInitCompleted()).toBeFalsy();
+
+    initCompletedSubject.next(true);
+
+    setTimeout(() => {
+      expect(component.hasInitCompleted()).toBeTruthy();
+      done();
+    });
+  });
+
+  // it(`should have the 'flye' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   // expect(app.title).toEqual('flye');
+  // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, flye');
+  // });
 });
--- src/app/classes/query-builder.spec.ts
+++ src/app/classes/query-builder.spec.ts
@@ -1,7 +1,18 @@
+import { QueryModel } from '../models/query.model';
 import { QueryBuilder } from './query-builder';
 
 describe('QueryBuilder', () => {
-  it('should create an instance', () => {
-    expect(new QueryBuilder()).toBeTruthy();
+  it('should return empty string when query is empty', () => {
+    const query = {};
+    const queryString = QueryBuilder.buildQueryParam(query);
+    expect(queryString).toEqual('');
+  });
+  it('should return useful query string when query is present', () => {
+    const query: QueryModel = {
+      page: 1,
+    };
+    const queryString = QueryBuilder.buildQueryParam(query);
+    expect(queryString.length > 0).toBeTruthy();
+    expect(queryString).toEqual('page=1');
   });
 });
