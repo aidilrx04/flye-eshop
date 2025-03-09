3d4df70cb4fababd107f4514612ed2b802b81342
aidilrx04 <mrcrayv14@gmail.com>
2025-03-09 15:32:19 +0800

    modal component test

--- src/app/components/core/modal/modal.component.spec.ts
+++ src/app/components/core/modal/modal.component.spec.ts
@@ -1,16 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalComponent } from './modal.component';
+import { ModalService } from '../../../services/modal.service';
+import { of } from 'rxjs';
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
   let fixture: ComponentFixture<ModalComponent>;
+  let modalServiceMock: jasmine.SpyObj<ModalService>;
 
   beforeEach(async () => {
+    modalServiceMock = jasmine.createSpyObj<ModalService>(
+      'ModalService',
+      ['closeCurrentModal'],
+      {
+        modal$: of(null),
+      },
+    );
     await TestBed.configureTestingModule({
-      imports: [ModalComponent]
-    })
-    .compileComponents();
+      imports: [ModalComponent],
+      providers: [
+        {
+          provide: ModalService,
+          useValue: modalServiceMock,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ModalComponent);
     component = fixture.componentInstance;
@@ -19,5 +34,14 @@ describe('ModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close a modal', () => {
+    component.closeCurrentModal();
+    expect(modalServiceMock.closeCurrentModal).toHaveBeenCalled();
+  });
+  it('should close a modal from template', () => {
+    component.closeModalTemplate();
+    expect(modalServiceMock.closeCurrentModal).toHaveBeenCalled();
   });
 });
