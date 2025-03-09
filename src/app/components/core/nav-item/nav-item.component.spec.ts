aca77d844f8ecda98659770b529d41a9c81724c2
aidilrx04 <mrcrayv14@gmail.com>
2025-03-09 15:36:00 +0800

    nav item test

--- src/app/components/core/nav-item/nav-item.component.spec.ts
+++ src/app/components/core/nav-item/nav-item.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavItemComponent } from './nav-item.component';
+import { NavItemType } from './nav-item-type';
+import { provideRouter } from '@angular/router';
 
 describe('NavItemComponent', () => {
   let component: NavItemComponent;
@@ -8,16 +10,33 @@ describe('NavItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavItemComponent]
-    })
-    .compileComponents();
+      imports: [NavItemComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavItemComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be false if item is not a header', () => {
+    fixture.componentRef.setInput('item', {
+      type: NavItemType.LINK,
+    });
+    fixture.detectChanges();
+
+    expect(component.isHeader).toBeFalsy();
+  });
+  it('should be treu if item is a header', () => {
+    fixture.componentRef.setInput('item', {
+      type: NavItemType.HEADER,
+    });
+    fixture.detectChanges();
+
+    expect(component.isHeader).toBeTruthy();
   });
 });
